36797fa2b9599fadd3e29090590f141b3b14d0b7
LeeSF03 <shuenfeilee@gmail.com>
2024-09-22 01:24:56 +0800

    fix: git branch not updating on every branch

--- git-prompt.sh
+++ git-prompt.sh
@@ -9,6 +9,13 @@ parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [ \1 ]/'
 }
 
+turquoise="\[\e[0;36m\]"
+bold_blue="\[\e[1;34m\]"
+red="\[\e[0;31m\]"
+blue="\[\033[0m\]"
+purple="\[\033[35m\]"
+yellow="\[\e[0;33m\]"
+
 if test -f ~/.config/git/git-prompt.sh
 then
 	. ~/.config/git/git-prompt.sh
@@ -35,8 +42,8 @@ else
 		then
 			. "$COMPLETION_PATH/git-completion.bash"
 			. "$COMPLETION_PATH/git-prompt.sh"
-			PS1="$PS1"'\[\033[36m\]'          # change color to turquoise
-			PS1="$PS1""$(parse_git_branch)"   # bash function
+			PS1="$PS1"'\[\e[0;36m\]'          # change color to turquoise
+			PS1="$PS1 $(parse_git_branch)"   # bash function
 		fi
 	fi
 	PS1="$PS1"'\[\e[0;36m\]'        # change color to turquoise
@@ -46,6 +53,26 @@ else
 fi
 
 MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
+
+prompt_command() {
+	if test -z "$WINELOADERNOEXEC"
+	then
+		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+		if test -f "$COMPLETION_PATH/git-prompt.sh"
+		then
+			. "$COMPLETION_PATH/git-completion.bash"
+			. "$COMPLETION_PATH/git-prompt.sh"
+			PS1="\[\033]0;Git | Bash v\v | \W\007\]\n\[\e[0;36m\]>> \[\e[1;34m\][ \u ]\[\e[0;31m\][ \@ ]\[\033[0m\][ \d ]\[\033[35m\][ \w ]\[\e[0;36m\]$(parse_git_branch)\n>> \[\e[0;33m\]"
+		fi
+	else
+		PS1="\[\033]0;Git | Bash v\v | \W\007\]\n\[\e[0;36m\]>> \[\e[1;34m\][ \u ]\[\e[0;31m\][ \@ ]\[\033[0m\][ \d ]\[\033[35m\][ \w ]\[\e[0;36m\]\n>> \[\e[0;33m\]"
+	fi
+}
+
+PROMPT_COMMAND=prompt_command
 
 # Evaluate all user-specific Bash completion scripts (if any)
 if test -z "$WINELOADERNOEXEC"
